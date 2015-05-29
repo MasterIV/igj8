a256900f9991a277e1ccb8e2e2bec2e2dba19465
purhur <purhur@purh.pw>
2015-05-29 22:49:49 +0200

    Commit

--- js/entities/TestBox.js
+++ js/entities/TestBox.js
@@ -2,20 +2,28 @@ function TestBox(x, y, world) {
 	this.position = new V2(x,y);
 
 	this.bodyDef = new Box2D.b2BodyDef;
-	this.bodyDef.type = Box2D.b2_dynamicBody;
-	//this.bodyDef.position.set(this.position.x, this.position.y);
-	this.bodyDef.angle = 0;
+	this.bodyDef.set_type(Box2D.b2_dynamicBody);
+	this.bodyDef.set_position(this.position.x, this.position.y);
+	this.bodyDef.set_angle(0);
 
-	//this.body = new Box2D.b2Body(this.bodyDef);
+
+	this.body = world.CreateBody(world);
+
+	this.polygonShape = new Box2D.b2PolygonShape;
+	this.polygonShape.SetAsBox(1,1);
 
 	this.fixDef = new Box2D.b2FixtureDef;
+	this.fixDef.set_shape(this.polygonShape);
+	this.fixDef.set_density(1);
+
+	this.fixture = this.body.CreateFixture(this.fixDef);
 }
 
 TestBox.prototype = new Box2DEntity;
 
 
 TestBox.prototype.draw = function ( ctx ) {
-	//...
+	ctx.rect(this.body.get_position().x, this.body.get_position().y, 10,10)
 }
 
 TestBox.prototype.update = function ( delta ) {
