a0c4f2a57821ce8c0afc9afe8ce79a9c1003ffa7
Tobias Rojahn <tobias@rojahn.org>
2015-05-29 20:46:56 +0200

    Framework fixes

--- js/ui/buttons.js
+++ js/ui/buttons.js
@@ -1,23 +1,23 @@
 /* Image Button */
-function button( img, hover, x, y, callback, sound ) {
-	this.img = new sprite( img );
-	this.hover = new sprite( hover );
+function Button( img, hover, x, y, callback, sound ) {
+	this.img = new Sprite( img );
+	this.hover = new Sprite( hover );
 	this.sound = sound;
-	this.area = new Rect( new V2( x, y ), new V2( x+this.image.width, y+this.image.height ));
+	this.area = new Rect( new V2( x, y ), new V2( x+this.img.width, y+this.img.height ));
 	this.callback = callback;
 	this.x = x;
 	this.y = y;
 }
 
-button.prototype.draw = function( ctx ) {
+Button.prototype.draw = function( ctx ) {
 	if( this.hover &&  this.area.inside( mouse )) this.hover.draw( ctx, this.x, this.y );
 	else this.img.draw( ctx, this.x, this.y );
-}
+};
 
 /* Sprite button */
 
-function spriteButton( img, position, hover, x, y, callback, sound ) {
-	this.img = new sprite( img );
+function SpriteButton( img, position, hover, x, y, callback, sound ) {
+	this.img = g[img];
 	this.position = position;
 	this.hover = hover;
 	this.sound = sound;
@@ -27,30 +27,32 @@ function spriteButton( img, position, hover, x, y, callback, sound ) {
 	this.y = y;
 }
 
-spriteButton.prototype.draw = function( ctx ) {
+SpriteButton.prototype.draw = function( ctx ) {
 	if( this.hover &&  this.area.inside( mouse )) {
-		this.img.draw( ctx,
+		ctx.drawImage( this.img,
 				this.hover.p1.x, this.hover.p1.y, this.hover.width(), this.hover.height(),
 				this.x, this.y, this.hover.width(), this.hover.height());
 	} else {
-		this.img.draw( ctx,
+		ctx.drawImage( this.img,
 				this.position.p1.x, this.position.p1.y, this.position.width(), this.position.height(),
 				this.x, this.y, this.position.width(), this.position.height());
 	}
-}
+};
 
 /* Text Button */
 
-function textButton( text, x, y, w, h, colors, hover, sound ) {
+function TextButton( text, x, y, w, h, callback, colors, hover, sound ) {
 	this.area = new Rect( new V2( x, y ), new V2( x+w, y+h));
 	this.text = text;
 	this.colors = colors;
 	this.hover = hover;
 	this.sound = sound;
+	this.callback = callback;
 }
 
-textButton.prototype.draw = function( ctx ) {
+TextButton.prototype.draw = function( ctx ) {
 	var c = this.hover &&  this.area.inside( mouse ) ? this.hover : this.colors;
+	ctx.font = '24px monospace';
 
 	ctx.fillStyle = c.background ? c.background : '#EEEEEE';
 	ctx.fillRect( this.area.p1.x, this.area.p1.y, this.area.width(), this.area.height());
@@ -58,15 +60,15 @@ textButton.prototype.draw = function( ctx ) {
 	ctx.strokeStyle = c.border ? c.border : 'black';
 	ctx.strokeRect( this.area.p1.x, this.area.p1.y, this.area.width(), this.area.height());
 
-	ctx.strokeStyle = c.text ? c.text : 'black';
-	ctx.fillText( this.text, this.area.p1.x, this.area.p1.y, this.area.width());
-}
+	ctx.fillStyle = c.text ? c.text : 'black';
+	ctx.fillText( this.text, this.area.p1.x+10, this.area.p1.y+20, this.area.width());
+};
 
 /* Shared Source */
 
-textButton.prototype.click =
-button.prototype.click =
-spriteButton.prototype.click = function( pos ) {
+TextButton.prototype.click =
+Button.prototype.click =
+SpriteButton.prototype.click = function( pos ) {
 	if( this.area.inside( pos )) {
 		if( this.sound ) sound.play(this.sound);
 		this.callback();
