444d42dfa9ce60efd1dd4a57d6d8998242d8af14
purhur <purhur@purh.pw>
2015-05-30 10:18:47 +0200

    Commit

--- js/entities/CarrierShip.js
+++ js/entities/CarrierShip.js
@@ -3,27 +3,24 @@ function CarrierShip(x, y, world, definition) {
 	this.sprite = new Sprite('img/mothership.png');
 
 
-
-	this.bodyDef = new Box2D.b2BodyDef;
-	this.bodyDef.set_type(Box2D.b2_kinematicBody);
-	this.bodyDef.set_position(new Box2D.b2Vec2(x,y));
-	this.bodyDef.set_angle(0);
+	this.bodyDef = new b2BodyDef();
+	this.bodyDef.position.set(x,y);
 
 
 	this.body = world.CreateBody(this.bodyDef);
 
-	this.polygonShape = new Box2D.b2PolygonShape;
-	this.polygon.SetAsBox(1,1);
+	this.polygonShape = new b2PolygonShape;
+	//this.polygon.SetAsBox(1,1);
 	this.points = [];
 	for(var i =0;i<definition.polygon[0].length;i++) {
 		console.log(definition.polygon[0][i].x,definition.polygon[0][i].y);
-		this.points.push(new Box2D.b2Vec2(definition.polygon[0][i].x,definition.polygon[0][i].y));
+		this.points.push(new b2Vec2(definition.polygon[0][i].x,definition.polygon[0][i].y));
 	}
 
 	console.log(this.points);
-	//this.polygonShape.Set(this.points, this.points.length);
+	this.polygonShape.Set(this.points, this.points.length);
 
-	this.fixDef = new Box2D.b2FixtureDef;
+	this.fixDef = new b2FixtureDef;
 	this.fixDef.set_shape(this.polygonShape);
 
 
@@ -32,11 +29,11 @@ function CarrierShip(x, y, world, definition) {
 	//this.fixDef.set_density(1);
 	//this.fixDef.set_mass(1);
 
-	this.body.SetLinearVelocity( new Box2D.b2Vec2( -10, 0 ));
+	this.body.SetLinearVelocity( new b2Vec2( -10, 0 ));
 	//this.body.ResetMassData();
 
 	//
-	//this.body.ApplyLinearImpulse(new Box2D.b2Vec2(xspeed,yspeed));
+	//this.body.ApplyLinearImpulse(new b2Vec2(xspeed,yspeed));
 
 
 	this.fixture = this.body.CreateFixture(this.fixDef);
@@ -53,6 +50,6 @@ CarrierShip.prototype.draw = function ( ctx ) {
 
 CarrierShip.prototype.update = function ( delta ) {
 	//...
-	//this.body.ApplyForce(new Box2D.b2Vec2(300,200));
+	//this.body.ApplyForce(new b2Vec2(300,200));
 
 }
