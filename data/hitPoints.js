d900db15188560f553990651b2019c295e0525a3
askat86 <askat@hab-verschlafen.de>
2015-05-31 13:10:41 +0200

    Hitpoints graphics

--- data/hitPoints.js
+++ data/hitPoints.js
@@ -4,13 +4,15 @@ var hitPoints = {
 		description: 'Lorem Ipsum',
 		sprite:'img/hitpoint_green.png',
 		hp:40,
+		armored:false,
 		offset:{x:-0,y:0}
 	},
 	point2: {
 		title: 'Hitpoint2',
 		description: 'Lorem Ipsum',
-		sprite:'img/hitpoint_blue.png',
+		sprite:'img/hitpoint_yellow.png',
 		hp:80,
+		armored:false,
 		offset:{x:-0,y:0}
 	},
 	point3: {
@@ -18,13 +20,23 @@ var hitPoints = {
 		description: 'Lorem Ipsum',
 		sprite:'img/hitpoint_red.png',
 		hp:120,
+		armored:false,
 		offset:{x:-0,y:0}
 	},
 	point4: {
 		title: 'Hitpoint4',
 		description: 'Lorem Ipsum',
-		sprite:'img/hitpoint_red_fortified.png',
+		sprite:'img/hitpoint_yellow_fortified.png',
 		hp:60,
+		armored:true,
+		offset:{x:-0,y:0}
+	},
+	point5: {
+		title: 'Hitpoint5',
+		description: 'Lorem Ipsum',
+		sprite:'img/hitpoint_red_fortified.png',
+		hp:100,
+		armored:true,
 		offset:{x:-0,y:0}
 	}
 };
