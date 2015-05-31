f49d5cf5faad545884ad685295e1bad8f1dab558
Armag3ddon <Armag3ddon@web.de>
2015-05-31 12:46:06 +0200

    dhdhdh

--- js/scenes/MenuScene.js
+++ js/scenes/MenuScene.js
@@ -2,7 +2,7 @@ function MenuScene() {
 	this.bg = new Sprite('img/background1.jpg');
 
 	this.entities = [ new Button('img/button_blank.png', 'img/button_blank.png', 1280/2 - 374/2, 720/2 -70/2, function() {
-						  game.scene = new PreUniverseScene(levels.level0);
+						  game.nextLevel();
 					  }, 'sounds/menu/button_clicked.mp3'),
 						new StaticSprite('img/button_start_game.png',1280/2, 720/2)
 					 ];
