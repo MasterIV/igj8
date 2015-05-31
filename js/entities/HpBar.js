4a8d480be0e5fb8c49ed7fcdfcdf8f342143d824
Tobias Rojahn <tobias@rojahn.org>
2015-05-31 14:39:25 +0200

    flash red

--- js/entities/HpBar.js
+++ js/entities/HpBar.js
@@ -13,6 +13,7 @@ HpBar.prototype.setHp = function ( hp ) {
 
 
 HpBar.prototype.reduce = function ( hp ) {
+	game.scene.entities.push( new ScreenFlash( 300 ));
 	if (typeof hp == 'undefined') hp = 1
 	this.hp -= hp;
 	if( this.hp < 1 )
